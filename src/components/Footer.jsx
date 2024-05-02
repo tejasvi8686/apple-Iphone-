8e9b9134b83e4179b5fbfd7a4430c38b75e83a5d
tejavi8686 <tejasviraj8686@gmail.com>
2024-05-02 18:56:07 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { footerLinks } from '../constants'
+import React from "react";
+import { footerLinks } from "../constants";
+
 
 const Footer = () => {
   return (
@@ -7,15 +8,10 @@ const Footer = () => {
       <div className="screen-max-width">
         <div>
           <p className="font-semibold text-gray text-xs">
-            More ways to shop: {' '}
-            <span className="underline text-blue">
-            Find an Apple Store {' '}
-            </span>
-            or {' '}
-            <span className="underline text-blue">
-            other retailer
-            </span>{' '}
-            near you.
+            More ways to shop:{" "}
+            <span className="underline text-blue">Find an Apple Store </span>
+            or <span className="underline text-blue">other retailer</span> near
+            you.
           </p>
           <p className="font-semibold text-gray text-xs">
             Or call 000800-040-1966
@@ -25,11 +21,23 @@ const Footer = () => {
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <p className="font-semibold text-gray text-xs">
+            Copright @ 2024 Apple Inc. All rights reserved.
+          </p>
+          <p className="font-semibold text-gray text-xs">
+            Crafted with passion by {" "}
+            <a
+              href="https://tejasviraj.vercel.app/"
+              className="font-black"
+              target="_blank"
+            >
+              Tejasvi Raj
+            </a>
+          </p>
           <div className="flex">
             {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
-                {link}{' '}
+                {link}{" "}
                 {i !== footerLinks.length - 1 && (
                   <span className="mx-2"> | </span>
                 )}
@@ -39,7 +47,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
